4b030f3e085178d790b35aa5999eb405f7a7c492
randyparies <97298915+randyparies@users.noreply.github.com>
2022-02-28 08:17:43 -0700

    fix php errror

--- postInstall.php
+++ postInstall.php
@@ -3,7 +3,7 @@
 echo "THIS IS A TEST\n";
 error_log("**** THIS IS A TEST\n",3,"/tmp/composer.debug");
 
-echo "-->". print_r($argv,true). "\n");
+echo "-->". print_r($argv,true). "\n";
 
 error_log(print_r($argv,true)."\n",3,"/tmp/composer.debug");
 
