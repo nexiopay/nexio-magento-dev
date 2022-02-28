4c558cf4ac0a8cf89acca059bc965dbd11dc52f0
randyparies <97298915+randyparies@users.noreply.github.com>
2022-02-28 08:11:49 -0700

    some debugging

--- postInstall.php
+++ postInstall.php
@@ -1,5 +1,9 @@
 <?php
 
 echo "THIS IS A TEST\n";
-error_log("THIS IS A TEST",3,"/tmp/composer.debug");
+error_log("**** THIS IS A TEST\n",3,"/tmp/composer.debug");
+
+echo "-->". print_r($argv,true). "\n");
+
+error_log(print_r($argv,true)."\n",3,"/tmp/composer.debug");
 
