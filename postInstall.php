0fa439103512586acc45f5b7e1d22d23676fbc10
Nexio Developer <developer@nex.io>
2022-03-01 19:27:13 +0000

    building v0.0.23

--- postInstall.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-echo "THIS IS A TEST\n";
-error_log("**** THIS IS A TEST\n",3,"/tmp/composer.debug");
-
-echo "-->". print_r($argv,true). "\n";
-
-error_log(print_r($argv,true)."\n",3,"/tmp/composer.debug");
-
