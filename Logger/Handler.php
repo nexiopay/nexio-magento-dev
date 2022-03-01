9bfddbd29b6ae64545919426a546f2730108272a
Nexio Developer <developer@nex.io>
2022-03-01 18:18:33 +0000

    building v0.0.22

---
+++ Logger/Handler.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Nexio Credit Card Logger Handler
+ *
+ * NOTICE OF LICENSE
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade nexio to newer
+ * versions in the future. If you wish to customize nexio for your
+ * needs please refer to Nexio for more information.
+ *
+ * PHP version 7.4
+ *
+ * @category  Nexio
+ * @package   Nexio_OnlinePayment
+ * @author    Nexio <kbatchelor@cmsonline.com>
+ * @copyright 2020-2022 Nexio
+ * @license   https://nex.io Nexio Licence
+ * @link      https://nex.io
+ */
+
+namespace Nexio\OnlinePayment\Logger;
+
+/**
+ * Class Handler
+ *
+ * @category  Nexio
+ * @package   Nexio_OnlinePayment
+ * @author    Nexio <kbatchelor@cmsonline.com>
+ * @copyright 2020-2022 Nexio
+ * @license   https://nex.io Nexio Licence
+ * @link      https://nex.io
+ */
+class Handler extends \Magento\Framework\Logger\Handler\Base
+{
+    /**
+     * Logging level
+     *
+     * @var int
+     */
+    protected $loggerType = Logger::INFO;
+
+    /**
+     * File name
+     *
+     * @var string
+     */
+    protected $fileName = '/var/log/nexioApiRequestResponse.log';
+}
---
+++ Model/ResourceModel/NexioLog.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Nexio Credit Card module Model
+ *
+ * NOTICE OF LICENSE
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade nexio to newer
+ * versions in the future. If you wish to customize nexio for your
+ * needs please refer to Nexio for more information.
+ *
+ * PHP version 7.4
+ *
+ * @category  Nexio
+ * @package   Nexio_OnlinePayment
+ * @author    Nexio <kbatchelor@cmsonline.com>
+ * @copyright 2020-2022 Nexio
+ * @license   https://nex.io Nexio Licence
+ * @link      https://nex.io
+ */
+
+namespace Nexio\OnlinePayment\Model\ResourceModel;
+
+use Magento\Framework\Model\ResourceModel\Db\AbstractDb;
+
+/**
+ * Class NexioLog
+ *
+ * @category  Nexio
+ * @package   Nexio_OnlinePayment
+ * @author    Nexio <kbatchelor@cmsonline.com>
+ * @copyright 2020-2022 Nexio
+ * @license   https://nex.io Nexio Licence
+ * @link      https://nex.io
+ */
+class NexioLog extends AbstractDb
+{
+    /**
+     * Initialize resource model
+     *
+     * @return void Analytics records
+     */
+    protected function _construct()
+    {
+        $this->_init('nexio_analytics', 'analytics_id');
+    }
+}
